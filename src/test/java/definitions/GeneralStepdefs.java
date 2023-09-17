de8c986af1cb86b26e734a9adaa38a639b98a5e2
dmitry <dmitry.ulyanichev@gmail.com>
2023-09-17 19:42:06 +0200

    Initial commit

---
+++ src/test/java/definitions/GeneralStepdefs.java
@@ -0,0 +1,19 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import pages.HomePage;
+
+public class EmptyStepdefs {
+
+    HomePage homePage = new HomePage();
+    @Given("I open google")
+    public void iOpenGoogle() {
+        homePage.open();
+    }
+
+    @Then("I wait {int} sec")
+    public void iWaitSec(int sec) throws InterruptedException {
+        Thread.sleep(sec * 1000);
+    }
+}
